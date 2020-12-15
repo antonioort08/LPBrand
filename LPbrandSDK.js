0a7f18e11d171582f9c705d8863d56b411cf8863
antonioort08 <65745969+antonioort08@users.noreply.github.com>
2020-12-15 10:35:07 -0600

    Update LPbrandSDK.js

--- LPbrandSDK.js
+++ LPbrandSDK.js
@@ -21,6 +21,7 @@ this.token||(this.token=b.data);this.disposed||!this.hosted||this.ready||(d=!0)}
 });
 
     var onSuccess = function(data) {
+        
         if (data[data.length - 1].source != 'agent') {
             var q = data[data.length - 1].text;
             var e = document.getElementById('info')
@@ -28,7 +29,8 @@ this.token||(this.token=b.data);this.disposed||!this.hosted||this.ready||(d=!0)}
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                    // Typical action to be performed when the document is ready:
-                   document.getElementById("info").innerHTML = xhttp.responseText;
+                  // document.getElementById("info").innerHTML = xhttp.responseText;
+                    document.getElementById("info").innerHTML = data;
                 }
             };
             xhttp.open("GET", `https://www.omdbapi.com/?t=${q}&apikey=11291606`, true);
